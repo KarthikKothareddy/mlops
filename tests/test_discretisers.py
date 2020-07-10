6a217f2ab7e37c262a949510777e4ab2c4883607
Karthik Kothareddy <karthikreddy.kp@gmail.com>
2020-07-10 08:11:34 -0500

    survival analysis init

---
+++ tests/test_discretisers.py
@@ -0,0 +1,52 @@
+
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from src.discretiser.guided import DecisionTreeDiscretiser
+
+
+def impute_na(data, variable):
+    # function to fill NA with a random sample
+    df = data.copy()
+    # random sampling
+    df[variable+'_random'] = df[variable]
+    # extract the random sample to fill the na
+    random_sample = df[variable].dropna().sample(
+        df[variable].isnull().sum(), random_state=0)
+    # pandas needs to have the same index in order to merge datasets
+    random_sample.index = df[df[variable].isnull()].index
+    df.loc[df[variable].isnull(), variable+'_random'] = random_sample
+
+    return df[variable+'_random']
+
+
+def test_decision_tree_discretiser():
+    data = pd.read_csv(
+        "../data/titanic.csv",
+        usecols=['age', 'fare', 'survived']
+    )
+    x_train, x_test, y_train, y_test = train_test_split(
+        data[['age', 'fare']],
+        data['survived'],
+        test_size=0.3,
+        random_state=0
+    )
+    # replace NA in both train and test sets
+    x_train['age'] = impute_na(data, 'age')
+    x_train['fare'] = impute_na(data, 'fare')
+    x_test['age'] = impute_na(data, 'age')
+    x_test['fare'] = impute_na(data, 'fare')
+
+    discretiser = DecisionTreeDiscretiser(
+        features=["age"], cv=10
+    )
+
+    discretiser.fit(x_train, y_train)
+    print(discretiser.binner_dict_)
+    tmp = discretiser.transform(x_train)
+    print(tmp.age.unique())
+
+
+test_decision_tree_discretiser()
+
